a86ce20852185f09880d8b1ac6b25198f55238cc
vd89 <me@daxcodes.in>
2020-05-12 08:32:29 +0530

    add the alternating numbers in an array

---
+++ alternatingSums/app.js
@@ -0,0 +1,14 @@
+function alternatingSums(a) {
+  let evenSum = 0;
+  let oddSum = 0;
+  a.forEach((element, index) => {
+    if (index % 2 == 0) {
+      evenSum += element;
+    } else {
+      oddSum += element;
+    }
+  });
+
+  return [evenSum, oddSum];
+}
+console.log(alternatingSums([50, 60, 60, 45, 70]));
